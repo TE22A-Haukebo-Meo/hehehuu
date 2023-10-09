22fe2aaefdef31bf062f34113765f942d14525d6
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-10-09 08:40:58 +0200

    Uppgift a klar

--- src/App.java
+++ src/App.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        
+        System.out.println("Droppa ett feeeetttt tal");
+        Scanner tb = new Scanner(System.in);
+        int tal = tb.nextInt();
+        int split = tal/1000;
+        int klar = split*1000;
+        System.out.println(tal+" avrundat till närmaste tuental blir: "+klar);
     }
 }
